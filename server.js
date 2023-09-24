74109528f3231dc64bf4e973a6c6ba8186daf07e
Neil <Neil10241126@gmail.com>
2023-09-24 11:29:40 +0800

    push

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 // server.js
 const jsonServer = require('json-server')
+const auth = require('json-server-auth')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
+server.use(auth)
 server.use(router)
 server.listen(3000, () => {
   console.log('JSON Server is running')
